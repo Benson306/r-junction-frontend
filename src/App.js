2aa3efb5a752cf3e1066ba29c75a2ae2db9f876f
Benson Ndiwa <bnkimtai@gmail.com>
2024-08-19 14:45:35 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Search from "./components/Search";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gradient-to-l from-slate-800 via-slate-800 to-slate-900">
+      <Header />
+
+      <div className="text-4xl mt-10 mb-10 text-center text-white w-1/2 m-auto font-bold leading-relaxed tracking-wide font-montserrat">
+      Discover Your Next Role – Kenya’s Top Job Listings All in One Place
+      </div>
+      
+      <Search />
+      
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='flex justify-between bg-zinc-0 h-14 p-10 w-full'>
+      <div className='flex gap-1 items-center'>
+        <img src={require('../images/Maxi-Logo-cropped.png')} className='w-16 p-2'  />
+        <span className='text-white text-lg font-montserrat' >Recruitment Junction</span>
+      </div>
+      <div className='flex items-center gap-4 text-sm'>
+        <div>
+            <button className='text-white hover:text-zinc-400 font-montserrat'>Login</button>
+        </div>
+        <div>
+            <button className='bg-primary hover:bg-slate-900 p-2 rounded-lg text-white font-montserrat'>Employers / Post a job</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
